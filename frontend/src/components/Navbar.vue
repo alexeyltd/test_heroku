e3d10358f56e62c92195f338c8fe82db94f94306
Shuvaev Artem <sha@get-net.ru>
2020-02-14 00:47:04 +0500

    frontend changes 8.4

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -6,17 +6,20 @@
             </div>
             <md-toolbar class="md-transparent" md-elevation="0">
                 <!--<md-tabs class="md-primary" md-sync-route >-->
-                <md-button  id="tab-products" md-label="Home" to="/home" style="color: white">Home</md-button>
+                <md-button id="tab-products" md-label="Home" to="/home" style="color: white">Home</md-button>
+                <md-button id="tab-orders" md-label="My orders" to="/orders" style="color: white">My orders</md-button>
                 <md-button id="tab-faq" md-label="FAQ" to="/faq" style="color: white">FAQ</md-button>
-                <md-button id="tab-orders" md-label="My orders" to="/orders" style="color: white">Orders</md-button>
 
-                <div v-if="$store.state.login===true">
-                    <md-button id="tab-user" to="/account" style="color: white">{{this.user.first_name+' '+this.user.last_name}}
-                    </md-button>
-                </div>
-                <!--</md-tabs>-->
-                <div v-if="$store.state.login===false">
-                    <md-button to="/login" style="color: white">Sign in</md-button>
+                <div  class="md-toolbar-section-end">
+                    <div v-if="$store.state.login===true">
+                        <md-button id="tab-user" to="/account" style="color: white">{{this.user.first_name}}
+                            {{this.user.last_name}}
+                        </md-button>
+                    </div>
+                    <!--</md-tabs>-->
+                    <div v-if="$store.state.login===false">
+                        <md-button to="/login" style="color: white">Sign in</md-button>
+                    </div>
                 </div>
             </md-toolbar>
         </div>
