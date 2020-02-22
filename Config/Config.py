8f3c6628f9a60442210f16b32f0c2a60398486b7
Alexey Pavlenko <sk.alexey.pavlenko@gmail.com>
2020-02-22 16:57:21 +0200

    Update Config.py
    fixed

--- Config/Config.py
+++ Config/Config.py
@@ -1,6 +1,6 @@
 class Config:
     # pgsql address
-    SQLALCHEMY_DATABASE_URI = 'postgres://cubbgjperyjvjv:7f13fdcd80bb2583e25fe58e87870d2d0c2b0263e3189a39b9002892c4e9f546@ec2-54-247-82-14.eu-west-1.compute.amazonaws.com:5432/d3u8a447n5cr2s'
+    SQLALCHEMY_DATABASE_URI = 'postgres://gfbuvgqlcgotsz:ec60976958a339b9b6f78283ff2bb333bd40a4e8cfc29a8213983cf7de9beba3@ec2-34-193-42-173.compute-1.amazonaws.com:5432/dadb53j6pri57d'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     gmail_user = "exeltiveltd@gmail.com"
